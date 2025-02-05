6a545d511411dca12b60e401fd1eefc573b10953
rithikronald <rithikronald29@gmail.com>
2025-02-05 12:36:37 +0530

    Added Table to Reels Section

--- src/app/pages/reels/reels/index.jsx
+++ src/app/pages/reels/reels/index.jsx
@@ -1,13 +1,17 @@
+import { HRTable } from "components/custom/HRTable";
 import { Page } from "components/shared/Page";
 
 export default function Reels() {
   return (
     <Page title="Reelspage">
-      <div className="transition-content w-full px-[--margin-x] pt-5 lg:pt-6">
+      <div className="transition-content w-full px-[--margin-x] pt-5 lg:pt-6 mb-4">
         <div className="min-w-0">
           <h2 className="truncate text-xl font-medium tracking-wide text-gray-800 dark:text-dark-50">
             REELS Page
           </h2>
+          <div className="grid grid-cols-1 gap-4 sm:gap-5 lg:gap-6">
+            <HRTable />
+          </div>
         </div>
       </div>
     </Page>
